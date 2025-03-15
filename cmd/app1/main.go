2783137354e32284870c310eb5727a5ebd6e5091
Jynx <tbelanger099@gmail.com>
2025-03-15 18:18:51 -0400

    Creating the math module and adding a few basic functions

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "calculate/shared/utils"  // Importation du package utils
+	"calculate/shared/utils" // Importation du package utils
+	"fmt"
 )
 
 func main() {
-    fmt.Println("Hello from app1!")
-    utils.PrintHello()  // Appel à la fonction PrintHello du package utils
+	fmt.Println("Hello from app1!")
+	utils.PrintHello() // Appel à la fonction PrintHello du package utils
+
 }
---
+++ internal/math/math.go
@@ -0,0 +1,27 @@
+package math
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Subtract(a, b int) int {
+	return a - b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func Divide(a, b int) int {
+	if b == 0 {
+		return 0
+	}
+	return a / b
+}
+
+func Modulo(a, b int) int {
+	if b == 0 {
+		return 0
+	}
+	return a % b
+}
