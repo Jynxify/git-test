6cb9781653ea31cc70dc4bdfbe844a05dec53174
Jynx <tbelanger099@gmail.com>
2025-03-15 22:01:04 -0400

    Added a utils function to check if app is running as a docker container

--- shared/utils/utils.go
+++ shared/utils/utils.go
@@ -1,8 +1,17 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // Fonction pour afficher un message
 func PrintHello() {
-    fmt.Println("Hello from utils package")
+	fmt.Println("Hello from utils package")
+}
+
+// IsRunningInDocker checks if the application is running inside a Docker container
+func IsRunningInDocker() bool {
+	_, err := os.Stat("/.dockerenv")
+	return err == nil
 }
